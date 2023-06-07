908a9a478edd6688939e05428a94fd7cb1207c8a
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2023-06-07 20:01:08 +0200

    feat: add initial implementation of popup

--- src/popup.js
+++ src/popup.js
@@ -1,2 +1,23 @@
-import { S } from "./lib/sanctuary/mod.js";
-console.log("popup", S);
+import { log, S } from "./lib/sanctuary/mod.js";
+import { api, getCurrentTab, url2DID } from "./lib/identinet/mod.js";
+import { chainRej, encaseP, promise, reject, resolve } from "fluture";
+
+async function init() {
+  return S.pipe([
+    encaseP(() => getCurrentTab()),
+    S.map(log("url")),
+    S.map(([tabId, url]) => url2DID(url)),
+    S.map(S.pipe([
+      S.map(log("did")),
+      S.map(({ did, base_url }) => {
+        const el = document.getElementById("ssi-info");
+        el.innerText = did;
+        return el;
+      }),
+    ])),
+    promise,
+  ])("nix");
+  // el.innerHTML = "<ul><li>1</li><li>2</li></ul>";
+}
+
+init();
