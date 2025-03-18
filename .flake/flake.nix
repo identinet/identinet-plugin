99ce51dd3f596d4c39307b24906d327f54efd2a7
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2025-03-18 10:33:27 +0100

    chore(flake): update dependencies

--- .flake/flake.nix
+++ .flake/flake.nix
@@ -7,11 +7,18 @@
   # inputs.nixpkgs_oldchromium.url =
   #   "github:NixOS/nixpkgs?rev=e49c28b3baa3a93bdadb8966dd128f9985ea0a09";
   inputs.nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, nixpkgs_unstable, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs_unstable,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         unstable = nixpkgs_unstable.legacyPackages.${system};
@@ -20,7 +27,7 @@
           # Build dependencies
           firefox # Firefox browser
           just # Simple make replacement https://just.systems/
-          nodejs_20 # node used for husky installation https://nodejs.org/en/
+          nodejs_22 # node used for husky installation https://nodejs.org/en/
           chromium # Chromium browser
           unstable.nushell # Nu Shell https://www.nushell.sh/
 
@@ -36,18 +43,17 @@
           yq-go # YAML and JSON CLI processor https://mikefarah.gitbook.io/yq/
           jq # JSON CLI processor https://github.com/jqlang/jq
         ];
-        linuxOnlyPackages = with pkgs;
-          [
-            datree # kubernetes configuration validation and verification https://datree.io/
-          ];
-      in {
+        linuxOnlyPackages = with pkgs; [
+          datree # kubernetes configuration validation and verification https://datree.io/
+        ];
+      in
+      {
         devShell = pkgs.mkShell {
-          nativeBuildInputs = if pkgs.system == "x86_64-linux" then
-            allOsPackages ++ linuxOnlyPackages
-          else
-            allOsPackages;
+          nativeBuildInputs =
+            if pkgs.system == "x86_64-linux" then allOsPackages ++ linuxOnlyPackages else allOsPackages;
           buildInputs = [ ];
         };
 
-      });
+      }
+    );
 }
